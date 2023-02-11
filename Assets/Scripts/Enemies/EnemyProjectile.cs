8610da98bb6c2d8f87356968820dfd76aa7968e7
Keith Wilson <keithwilson@Keiths-MacBook-Air.local>
2023-02-11 17:46:11 -0500

    use_coroutine

--- Assets/Scripts/Enemies/EnemyProjectile.cs
+++ Assets/Scripts/Enemies/EnemyProjectile.cs
@@ -7,14 +7,5 @@ public class EnemyProjectile : MonoBehaviour
     // Start is called before the first frame update
     public  AttackData projectileAttackData;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 }
