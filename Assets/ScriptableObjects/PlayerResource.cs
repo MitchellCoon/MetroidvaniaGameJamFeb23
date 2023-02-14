45d560cbd9cd735dacd5559276f174e2acbc2d53
MitchellCoon <46406678+MitchellCoon@users.noreply.github.com>
2023-02-14 18:45:51 -0500

    Added health counter

--- Assets/ScriptableObjects/PlayerResource.cs
+++ Assets/ScriptableObjects/PlayerResource.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 [CreateAssetMenu(fileName = "New Player Resource", menuName = "PlayerResource")]
 
@@ -9,11 +11,16 @@ public class PlayerResource : ScriptableObject
     [SerializeField] new string name;
     [SerializeField] int maxValue;
     [SerializeField] int minValue;
+    [SerializeField] int canvasChildNumber;
+    [SerializeField] string staticText;
+
     private int currentValue;
+    private Canvas canvas;
 
     private void OnEnable()
     {
         currentValue = maxValue;
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
     }
 
     public int GetCurrentValue()
@@ -21,14 +28,21 @@ public class PlayerResource : ScriptableObject
         return currentValue;
     }
 
+    public void UpdateUIElement()
+    {
+        canvas.transform.GetChild(canvasChildNumber).GetComponent<TextMeshProUGUI>().text = staticText + currentValue;
+    }
+
     public void AddResource(int amount)
     {
         currentValue = Mathf.Min(maxValue, currentValue + amount);
+        UpdateUIElement();
     }
 
     public void SubtractResource(int amount)
     {
         currentValue = Mathf.Max(minValue, currentValue - amount);
+        UpdateUIElement();
     }
 
 }
