b7e3ffd2e8730a01b4a009436f912533876da595
Don Juan Javier <townofdon@gmail.com>
2023-02-19 17:10:50 -0500

    add map refinements
    - add support for tilemaps of arbitrary scales
    - center map to room position on room load
    - fix map generation script crashing for brand-new image generation (null importer)
    - add first GlobalEvent usage
    - turn off map camera when not in-use

--- Assets/Scripts/Core/GlobalEvent.cs
+++ Assets/Scripts/Core/GlobalEvent.cs
@@ -1,5 +1,18 @@
+using System;
+using UnityEngine;
+
+// The `event` keyword actually removes functionality from the Action, making it so that
+// we can only call `Invoke` from within the same class.
+
+// This does add some extra boilerplate but gives us more safety of not accidentally
+// overriding event subscriptions.
 
 public static class GlobalEvent
 {
+    public static event Action<Vector2> OnRoomLoaded;
 
+    public static class Invoke
+    {
+        public static void OnRoomLoaded(Vector2 roomPosition) { GlobalEvent.OnRoomLoaded?.Invoke(roomPosition); }
+    }
 }
--- Assets/Scripts/MapGen/WorldMapCamera.cs
+++ Assets/Scripts/MapGen/WorldMapCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace MapGen
@@ -9,9 +10,20 @@ namespace MapGen
 
         new Camera camera;
 
+        void OnEnable()
+        {
+            GlobalEvent.OnRoomLoaded += OnRoomLoaded;
+        }
+
+        void OnDisable()
+        {
+            GlobalEvent.OnRoomLoaded -= OnRoomLoaded;
+        }
+
         void Awake()
         {
             camera = GetComponent<Camera>();
+            camera.enabled = false;
         }
 
         void Update()
@@ -19,8 +31,20 @@ namespace MapGen
             // TODO: REMOVE THIS IN FAVOR OF PLAYER CONTROLLER + GLOBAL EVENT
             if (Input.GetButtonDown("Submit"))
             {
-                camera.depth *= -1;
+                camera.enabled = !camera.enabled;
             }
+        }
+
+        void SetCameraPosition(Vector3 incoming)
+        {
+            // preserve z value
+            incoming.z = camera.transform.position.z;
+            camera.transform.position = incoming;
+        }
+
+        void OnRoomLoaded(Vector2 roomPosition)
+        {
+            SetCameraPosition(roomPosition * 0.5f);
         }
     }
 }
