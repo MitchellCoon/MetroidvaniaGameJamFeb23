3731763d6168355a1a91c92ada783cd2c7744fab
Don Juan Javier <townofdon@gmail.com>
2023-02-25 14:32:16 -0500

    add ObjectEnableDisable - generic agnostic enabler

--- Assets/Scripts/Level/Editor/ObjectEnablerDisablerEditor.cs
+++ Assets/Scripts/Level/Editor/ObjectEnablerDisablerEditor.cs
@@ -8,9 +8,30 @@ public class ObjectEnablerDisablerEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
+
+
+        GUIStyle warningStyle = new GUIStyle();
+        warningStyle.normal.textColor = new Color(1, 0.66f, 0);
 
         var script = (ObjectEnablerDisabler)target;
+
+        if (!script.HasChildren || !script.HasCondition)
+        {
+            GUILayout.Space(20);
+        }
+
+        if (!script.HasChildren)
+        {
+            ErrorLabel("This GameObject requires one or more children");
+        }
+
+        if (!script.HasCondition)
+        {
+            ErrorLabel("Condition is required - add it below");
+        }
+
+        base.OnInspectorGUI();
+
 
         GUILayout.BeginVertical();
 
@@ -18,10 +39,19 @@ public class ObjectEnablerDisablerEditor : Editor
 
         GUILayout.Label("INSTRUCTIONS");
         GUILayout.Label("- Place children inside this GameObject");
-        GUILayout.Label("- Note - only top-level children are controlled");
         GUILayout.Label("- Add the BoolVariable you want to use as on/off state");
-        GUILayout.Label("That's it!");
+        GUILayout.Label("- Check `Invert` to evaluate BoolVar as its opposite value");
+        GUILayout.Space(20);
+        GUILayout.Label("*Note* - only top-level children are controlled", warningStyle);
 
         GUILayout.EndVertical();
+    }
+
+    void ErrorLabel(string text)
+    {
+        GUIStyle errorStyle = new GUIStyle();
+        errorStyle.normal.textColor = Color.red;
+        GUILayout.Label("!! " + text, errorStyle);
+        GUILayout.Space(20);
     }
 }
--- Assets/Scripts/Level/ObjectEnablerDisabler.cs
+++ Assets/Scripts/Level/ObjectEnablerDisabler.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -10,8 +11,58 @@ public class ObjectEnablerDisabler : MonoBehaviour
     public bool HasCondition => conditionToEnable != null && conditionToEnable.hasRef;
     public bool HasChildren => transform.childCount > 0;
 
+    List<GameObject> children = new List<GameObject>();
+
+    void OnEnable()
+    {
+        conditionToEnable.OnChanged += OnConditionChange;
+    }
+
+    void OnDisable()
+    {
+        conditionToEnable.OnChanged -= OnConditionChange;
+    }
+
     void Awake()
     {
-        Assert.IsTrue(transform.childCount > 0, "Please add one or more children to this component to enable/disable");
+        Assert.IsTrue(HasCondition, "Please add a condition to ObjectEnablerDisabler");
+        Assert.IsTrue(HasChildren, "Please add one or more children to this component to enable/disable");
+        PopulateChildren();
+    }
+
+    void Start()
+    {
+        OnConditionChange(false);
+    }
+
+    void PopulateChildren()
+    {
+        children.Clear();
+        foreach (Transform child in transform)
+        {
+            if (child != null) children.Add(child.gameObject);
+        }
+    }
+
+    void OnConditionChange(bool incoming)
+    {
+        if (conditionToEnable.value)
+        {
+            Activate();
+        }
+        else
+        {
+            Deactivate();
+        }
+    }
+
+    void Activate()
+    {
+        foreach (var child in children) if (child != null) child.SetActive(true);
+    }
+
+    void Deactivate()
+    {
+        foreach (var child in children) if (child != null) child.SetActive(false);
     }
 }
