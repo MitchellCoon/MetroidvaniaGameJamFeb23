e5ba05d2e76a67d5210d72b0b26febb4e3a82ee3
MitchellCoon <46406678+MitchellCoon@users.noreply.github.com>
2023-03-12 16:23:32 -0400

    Fixed issue where enemies teleport when falling platform respawns

--- Assets/FallingPlatform.cs
+++ Assets/FallingPlatform.cs
@@ -40,21 +40,21 @@ public class FallingPlatform : MonoBehaviour
     }
     void Restore()
     {
-        var tempPlayerObject =  GameObject.FindGameObjectWithTag("Player"); 
-        if(tempPlayerObject != null)
+        foreach (Transform child in transform)
         {
-            if(tempPlayerObject.transform.parent == this.transform){
-                tempPlayerObject.transform.parent = null;
+            if(!child.CompareTag(Constants.MOVING_PLATFORM_TAG)){
+                child.transform.parent = null;
             }
         }
+
         localRB.isKinematic = true;
         localRB.constraints = RigidbodyConstraints2D.FreezeAll;
         transform.position = objectOriginalPos;
         spriteObject.transform.position = spriteOriginalPos;
         elapsed = 0.0f;
         isShaking = false;
-       
     }
+
     IEnumerator OnCollisionEnter2D(Collision2D other)
     {   
         if(isShaking)
