4f3dab0c638ec4ae1edf76cd7b3b24b79ff11c64
MitchellCoon <46406678+MitchellCoon@users.noreply.github.com>
2023-03-16 16:26:18 -0400

    Update cat prefab and attack animation

--- Assets/Scripts/Enemies/JudgementCatEnd.cs
+++ Assets/Scripts/Enemies/JudgementCatEnd.cs
@@ -9,9 +9,12 @@ public class JudgementCatEnd : MonoBehaviour
     [SerializeField] Rigidbody2D body;
 
     [SerializeField] float chargeTime = 2f;
+    [SerializeField] float napTime = 4f;
 
     bool isCharging = false;
+    bool isNapping = false;
     float chargeTimer = 0f;
+    float napTimer = 0f;
     
 
     void Update()
@@ -20,8 +23,11 @@ public class JudgementCatEnd : MonoBehaviour
         {
             enemyAI.enabled = false;
             body.velocity = Vector3.zero;
+            chargeTimer = 0f;
             isCharging = true;
             animator.SetTrigger(Constants.JUDGEMENT_CAT_END_CHARGE_ANIMATION);
+            animator.SetBool(Constants.JUDGEMENT_CAT_END_SLASH_ANIMATION, false);
+            animator.SetBool(Constants.IS_MOVING_BOOL, false);
         }
         if (isCharging)
         {
@@ -32,6 +38,20 @@ public class JudgementCatEnd : MonoBehaviour
             isCharging = false;
             chargeTimer = 0f;
             animator.SetTrigger(Constants.JUDGEMENT_CAT_END_SLASH_ANIMATION);
+            napTimer = 0f;
+            isNapping = true;
+        }
+        if(isNapping)
+        {
+            napTimer += Time.deltaTime;
+            body.velocity = Vector3.zero;
+            animator.SetBool(Constants.IS_MOVING_BOOL, false);
+        }
+        if(napTimer > napTime)
+        {
+            enemyAI.enabled = true;
+            isNapping = false;
+            napTimer = 0f;
         }
     }
 }
